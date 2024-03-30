ef1658bf64eacca2e6ff96b2016f08192bd75306
Zinenko <zinkalexvvv@gmail.com>
2024-03-30 10:51:23 +0300

    commit

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,38 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarServiceImpl implements CarService {
+
+    private static int COUNT_CARS = 1;
+    private static List<Car> carsList;
+
+    {
+        carsList = new ArrayList<>();
+        carsList.add(new Car(COUNT_CARS++, "John", "Toyota", "12345"));
+        carsList.add(new Car(COUNT_CARS++,"Alice", "Honda", "67890"));
+        carsList.add(new Car(COUNT_CARS++,"Bob", "Ford", "54321"));
+        carsList.add(new Car(COUNT_CARS++,"Emily", "Chevrolet", "98765"));
+        carsList.add(new Car(COUNT_CARS++,"Mike", "Nissan", "13579"));
+    }
+
+    public List<Car> showAll() {return carsList;}
+
+    public List<Car> showCOUNT(int count) {
+       return carsList.stream().limit(count).toList();
+    }
+
+//    public static void main(String[] args) {
+//        CarServiceImpl csi = new CarServiceImpl();
+//        System.out.println(csi.showCOUNT(3));
+//    }
+
+
+}
+
+
